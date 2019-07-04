878d517348efa008b123c30e570ffb688b79155a
Matt Mets <matt@blinkinlabs.com>
2019-07-04 15:37:06 +0800

    Flash for 3 seconds on, 1 second off

--- firmware/main.c
+++ firmware/main.c
@@ -3,25 +3,20 @@
 #include <ch554.h>
 #include <debug.h>
 
-#define GREEN_LED_PIN 5
-#define OFFBOARD_LED_PIN 7
+#define OFFBOARD_LED_PIN 5
 
-SBIT(GREEN_LED, 0x90, GREEN_LED_PIN);
 SBIT(OFFBOARD_LED, 0x90, OFFBOARD_LED_PIN);
 
 void main() {
     CfgFsys();
-
-    P1_MOD_OC = P1_MOD_OC & ~(1<<GREEN_LED_PIN);
-    P1_DIR_PU = P1_DIR_PU | ~(1<<GREEN_LED_PIN);
+    mDelaymS(5);
 
     P1_MOD_OC = P1_MOD_OC & ~(1<<OFFBOARD_LED_PIN);
     P1_DIR_PU = P1_DIR_PU | ~(1<<OFFBOARD_LED_PIN);
 
-    GREEN_LED = 0;
-
     while (1) {
         OFFBOARD_LED = 1;
+
     	mDelaymS(200);
     	mDelaymS(200);
     	mDelaymS(200);
@@ -29,14 +24,25 @@ void main() {
     	mDelaymS(200);
     	mDelaymS(200);
     	mDelaymS(200);
+
+    	mDelaymS(200);
+    	mDelaymS(200);
+    	mDelaymS(200);
+    	mDelaymS(200);
+    	mDelaymS(200);
+    	mDelaymS(200);
+    	mDelaymS(200);
+
+    	mDelaymS(200);
+    	mDelaymS(200);
+    	mDelaymS(200);
+    	mDelaymS(200);
+    	mDelaymS(200);
+    	mDelaymS(200);
+    	mDelaymS(200);
+
         OFFBOARD_LED = 0;
-    	mDelaymS(200);
-    	mDelaymS(200);
-    	mDelaymS(200);
-    	mDelaymS(200);
-    	mDelaymS(200);
-    	mDelaymS(200);
-    	mDelaymS(200);
+
     	mDelaymS(200);
     	mDelaymS(200);
     	mDelaymS(200);
